3fd6666d098744749243fbdbffb486d89037d2f5
Jobathand6 <jonathandaussogne6@gmail.com>
2026-03-18 05:19:35 +0100

    fix layout typescript

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import { ThirdwebProvider } from "thirdweb/react";
 import "./globals.css";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
       <body>
